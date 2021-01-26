559416e8baaafff31349a93bd3f80640a15606b0
Mirko Brombin <send@mirko.pm>
2021-01-26 15:22:18 +0100

    Create build64.sh

---
+++ runners/chardonnay/build64.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# ---------------------------------------
+# build64.sh
+# This script build wine for 64bit
+# ---------------------------------------
+
+# Utilities
+# ---------------------------------------
+expand_bg="\e[K"
+red_bg="\e[0;101m${expand_bg}"
+green_bg="\e[0;102m${expand_bg}"
+black="\e[30m"
+bold="\e[1m"
+reset="\e[0m"
+
+function title {
+	PREFIX="\n$bold-----"
+	SUFFIX="--$reset"
+	echo -e "$PREFIX $1 $SUFFIX"
+}
+
+function print_execution {
+	if $1; then
+		echo -e "$green_bg$bold$black-- $1$reset"
+	else
+		echo -e "$red_bg$bold$black-- Operation failed for: $1$reset"
+	fi
+}
+
+# Configuring Wine 64
+# ---------------------------------------
+title "Configuring Wine 64"
+print_execution "cd ~/runner/work/wine/winesource_64"
+print_execution "./configure --prefix=/home/runner/work/winebuild_64"
+
+# Building Wine 64
+# ---------------------------------------
+title "Building Wine 64"
+print_execution "cd ~/runner/work/wine/winesource_64"
+print_execution "make -j$(nproc)"
+print_execution "make install"
