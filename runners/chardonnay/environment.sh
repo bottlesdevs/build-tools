368d69c5eaa6971384a7740d2b87947706c3fc3a
Mirko Brombin <send@mirko.pm>
2021-01-26 14:44:57 +0100

    Update environment.sh

--- runners/chardonnay/environment.sh
+++ runners/chardonnay/environment.sh
@@ -67,3 +67,8 @@ libvkd3d1 libvkd3d-dev libvkd3d-utils1 vkd3d-demos libvulkan1 \
 libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad \
 gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x \
 gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio liborc-0.4-dev"
+
+# Configuring GCC
+# ---------------------------------------
+title "Configuring GCC"
+if [ "$CXX" = "g++" ]; then export CXX="g++-5" CC="gcc-5"; fi
