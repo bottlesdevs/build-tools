7eb5a6a3ba8273106c826ae3fa97b6fc5e7e25c5
Mirko Brombin <send@mirko.pm>
2021-03-07 11:19:34 +0100

    Remove vkd3d-demos:i386

--- runners/chardonnay/environment.sh
+++ runners/chardonnay/environment.sh
@@ -71,7 +71,7 @@ libavutil-dev:i386 libswresample-dev:i386 libavcodec58:i386 libswresample3:i386 
 libfaudio-dev:i386 libvkd3d-dev:i386 libvkd3d-utils1:i386 libvulkan1:i386 libgstreamer1.0-0:i386 gstreamer1.0-libav:i386 \
 xserver-xorg-dev:i386 libfreetype6-dev:i386 gcc-multilib g++-multilib gcc-mingw-w64-i686 \
 mingw-w64-i686-dev libcloog-ppl1:i386 libvkd3d-shader1:i386 libgtk-3-dev:i386 \
-ocl-icd-opencl-dev:i386 linux-libc-dev:i386 libppl14:i386 libvkd3d1:i386 vkd3d-demos:i386 \ 
+ocl-icd-opencl-dev:i386 linux-libc-dev:i386 libppl14:i386 libvkd3d1:i386 \ 
 gstreamer1.0-plugins-good:i386 gstreamer1.0-plugins-bad:i386 gstreamer1.0-tools:i386 \
 gstreamer1.0-x:i386 gstreamer1.0-alsa:i386 gstreamer1.0-gl:i386 gstreamer1.0-gtk3:i386 \
 gstreamer1.0-qt5:i386 gstreamer1.0-pulseaudio:i386"
