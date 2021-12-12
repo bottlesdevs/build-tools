b57786375ed6a998264410e24598424e26f098dc
mirkobrombin <send@mirko.pm>
2021-12-12 22:26:12 +0100

    add runtime recipe

---
+++ runtime/build.py
@@ -0,0 +1,118 @@
+# ---------------------------------------
+# build.py
+# This script build the Bottles runtime
+# this is a set of librarires from Ubuntu latest to ensure compatibility
+# ---------------------------------------
+
+import os
+import shutil
+import json
+import tarfile
+
+def check_recipe():
+	'''
+	Check if the runtime recipe exists and load it, if not
+	raise an exception and exit
+	'''
+	if not os.path.exists('recipe.json'):
+		raise Exception('recipe.json not found')
+
+	return json.load(open('recipe.json'))
+
+
+def create_runtime_structure():
+	'''
+	Create the runtime directory structure where to store the
+	runtime libraries
+	'''
+	os.makedirs('runtime', exist_ok=True)
+	os.makedirs('runtime/lib32', exist_ok=True)
+	os.makedirs('runtime/lib64', exist_ok=True)
+ 
+
+def build_runtime(recipe):
+	'''
+	Build the runtime from the recipe, copy the libraries
+	from the current system to the runtime directory
+	'''
+	lib32 = []
+	lib64 = []
+	missed = []
+	
+	for lib in recipe['32bit'] + recipe['both']:
+		_res = search_library(lib, '32')
+		if _res:
+			lib32.append(_res)
+			break
+		else:
+			missed.append(lib)
+	
+	for lib in recipe['64bit'] + recipe['both']:
+		_res = search_library(lib, '64')
+		if _res:
+			lib64.append(_res)
+			break
+		else:
+			missed.append(lib)
+	
+	copy_to_runtime(lib32, '32')
+	copy_to_runtime(lib64, '64')
+	print(missed)
+
+
+def copy_to_runtime(libs, arch='32'):
+	'''
+	Copy the libraries to the runtime directory
+	'''
+	for lib in libs:
+		shutil.copy(lib, f'runtime/lib{arch}')
+
+
+def search_library(lib, arch='32'):
+	'''
+	Search for a library in the current system
+	'''
+	lookup32 = [
+		"/lib/i386-linux-gnu",
+		"/usr/lib/i386-linux-gnu",
+		"/usr/local/lib/i386-linux-gnu"  
+	]
+	lookup64 = [
+		"/lib/x86_64-linux-gnu",
+		"/usr/lib/x86_64-linux-gnu",
+		"/usr/local/lib/x86_64-linux-gnu"
+	]
+
+	lookup = lookup32
+	if arch == '64':
+		lookup = lookup64
+	
+	for path in lookup:
+		if os.path.exists(f'{path}/{lib}'):
+			return f'{path}/{lib}'
+	
+	return False
+
+
+def create_runtime_archive():
+	'''
+	Create the runtime archive
+	'''
+	tar = tarfile.open('runtime.tar.gz', 'w:gz')
+	tar.add('runtime')
+	tar.close()
+ 
+ 
+def cleanup():
+	'''
+	Cleanup the runtime directory
+	'''
+	shutil.rmtree('runtime')
+
+
+if __name__ == '__main__':
+	recipe = check_recipe()
+	create_runtime_structure()
+	build_runtime(recipe)
+	create_runtime_archive()
+	print('Runtime built!')
---
+++ runtime/environment.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# ---------------------------------------
+# environment.sh
+# This script configure the build environment
+# ---------------------------------------
+
+# Utilities
+# ---------------------------------------
+expand_bg="\e[K"
+red_bg="\e[0;101m${expand_bg}"
+green_bg="\e[0;102m${expand_bg}"
+black="\e[30m"
+bold="\e[1m"
+reset="\e[0m"
+
+function title {
+	PREFIX="\n$bold-----"
+	SUFFIX="--$reset"
+	echo -e "$PREFIX $1 $SUFFIX"
+}
+
+function print_execution {
+	if $1; then
+		echo -e "$green_bg$bold$black-- $1$reset"
+	else
+		echo -e "$red_bg$bold$black-- Operation failed for: $1$reset"
+	fi
+}
+
+# Enabling i386 architecture
+# ---------------------------------------
+title "Enabling i386 architecture"
+print_execution "sudo dpkg --add-architecture i386"
+
+# Adding repositories
+# ---------------------------------------
+title "Adding repositories"
+print_execution "sudo apt -y install software-properties-common"
+print_execution "sudo add-apt-repository ppa:cybermax-dexter/vkd3d -y"
+print_execution "sudo add-apt-repository ppa:cybermax-dexter/sdl2-backport -y"
+print_execution "sudo apt-get -qq update"
+
+# Installing i386 dependencies
+# ---------------------------------------
+title "Installing dependencies"
+print_execution "sudo apt install -o APT::Immediate-Configure=false -y \
+libsdl2-mixer-dev libsdl2-mixer-dev:i386\
+libnih-dbus-dev\
+libsoxr0 libsoxr0:i386\"
+
+title "APT fix"
+print_execution "sudo apt install -f -y"
+
