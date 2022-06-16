20bbec21f2cae3731a2528c8ff89dd1456d79a3c
Mirko Brombin <send@mirko.pm>
2022-06-16 17:10:36 +0200

    add BattlEye runtime

--- runtime/build.sh
+++ runtime/build.sh
@@ -46,3 +46,13 @@ print_execution "cd $HOME/runtime"
 print_execution "cp -a $HOME/work/runtime/runtime/EasyAntiCheatRuntime EasyAntiCheatRuntime"
 print_execution "cd .."
 print_execution "tar -czvf runtime.tar.gz runtime"
+
+# BE runtime
+# -------------------------------------------------
+# Note: support for manual files addition planned for runtimezilla
+title "Adding BE runtime"
+print_execution "rm $HOME/runtime.tar.gz"
+print_execution "cd $HOME/runtime"
+print_execution "cp -a $HOME/work/runtime/runtime/BattlEye BattlEye"
+print_execution "cd .."
+print_execution "tar -czvf runtime.tar.gz runtime"
