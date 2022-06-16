e4b592142455255674a4f0014b6eea74f8221ec8
Mirko Brombin <send@mirko.pm>
2022-06-16 10:47:45 +0200

    remove runtimezilla archive before updating

--- runtime/build.sh
+++ runtime/build.sh
@@ -41,6 +41,7 @@ print_execution "python3 runtimezilla --recipe ../recipe.yml --output $HOME/runt
 # -------------------------------------------------
 # Note: support for manual files addition planned for runtimezilla
 title "Adding EAC runtime"
+print_execution "rm $HOME/runtime/runtime.tar.gz"
 print_execution "cd $HOME/runtime"
 print_execution "cp -a $HOME/work/runtime/runtime/EasyAntiCheatRuntime EasyAntiCheatRuntime"
 print_execution "tar -czvf runtime.tar.gz runtime"
