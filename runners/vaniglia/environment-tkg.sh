1b9dd2c1371f832f8c0699059b4db956c66eb542
xioren <40043405+xioren@users.noreply.github.com>
2023-10-30 12:33:06 -0700

    Update environment-tkg.sh

--- runners/vaniglia/environment-tkg.sh
+++ runners/vaniglia/environment-tkg.sh
@@ -193,7 +193,6 @@ gstreamer1.0-plugins-good \
 gstreamer1.0-plugins-bad \
 gstreamer1.0-plugins-ugly \
 gstreamer1.0-libav \
-gstreamer1.0-doc \
 gstreamer1.0-tools \
 gstreamer1.0-x \
 gstreamer1.0-alsa \
